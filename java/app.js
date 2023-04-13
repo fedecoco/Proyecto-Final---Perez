b994309fd0136bfe7d990ee4c3c119a40cd958dd
fedecoco <fedem.perez05@gmail.com>
2023-04-13 18:28:13 -0300

    Prueba 5

--- java/app.js
+++ java/app.js
@@ -17,7 +17,9 @@ let score = 0;
 let timeLeft = 30;
 let bugInterval = 1000;
 let intervalId;
-
+let timerId;
+let gameOver = false;
+let speedIncreaseId;
 
 // Función para generar una cucaracha aleatoria
 function createBug() {
@@ -32,29 +34,31 @@ function createBug() {
     bugs.removeChild(bug);
   });
   bugs.appendChild(bug);
+
+  if (bugs.children.length >= 10) {
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'Perdiste hay PLAGAA!!.',
+    }).then(() => {
+      gameOver = true;
+      resetGame();
+    });
+  }
 }
-//Aumentar velocidad
+
+
 function increaseBugSpeed() {
   if (bugInterval > 500) { // velocidad máxima
     bugInterval -= 100;
     clearInterval(intervalId);
     intervalId = setInterval(() => {
-      createBug(); // cambiar a createBug()
+      createBug(); 
     }, bugInterval);
-    setTimeout(increaseBugSpeed, 5000);
-  }
-}
-// Funcion si perdes
-function generateBug() {
-  createBug();
-  if (bugs.children.length >= 10) {
-    Swal.fire({
-      icon: 'error',
-      title: 'Oops...',
-      text: 'Perdiste Hay PLAGAA!!.',
-    });
-  }
-}
+    speedIncreaseId = setTimeout(increaseBugSpeed, 5000);
+  } 
+} 
+
 
 //Funciond e guardar resultado
 function saveScore(name) {
@@ -78,7 +82,9 @@ function showScores() {
   const scores = JSON.parse(localStorage.getItem('scores')) || [];
   const scoreContainer = document.querySelector('.score-container');
   scoreContainer.innerHTML = ''; // Limpiar contenido anterior del elemento
-  if (scores.length <= 5) {
+  let users = [];
+  
+   if (scores.length <= 5) {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then(users => {
@@ -110,24 +116,6 @@ function showScores() {
 }
 
 
-// llamar cuando el juego se reinicio
-function resetGame() {
-  score = 0;
-  timeLeft = 30;
-  scoreEl.textContent = score;
-  timerEl.textContent = timeLeft;
-  clearInterval(intervalId);
-  clearInterval(timerId);
-  startButton.disabled = false;
-  bugs.querySelectorAll('.bug').forEach(bug => {
-    bugs.removeChild(bug);
-  });
-  showScores();
-  saveScore();
-  
-}
-
-
 
 // Función para mover la mano de gatito con el cursor del mouse
 function moveHand(e) {
@@ -147,25 +135,27 @@ function checkTime() {
     resetGame();
   }
 }
-// Función para generar una cucaracha y verificar si se quedan sin intentos
-function checkLives() {
-  const livesLeft = 10 - bugs.children.length;
-  if (livesLeft === 0) {    
-    Swal.fire({
-      icon: 'error',
-      title: 'Oops...',
-      text: 'Se acabaron los intentos. Fin del juego.',
-    });
-    resetGame();
+// función para generar una cucaracha y verificar si se quedan sin intentos
+function generateBug() {
+  if (!gameOver) {
+    createBug();
+    if (bugs.children.length >= 10) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Perdiste hay PLAGAA!!.',
+      }).then(() => {
+        gameOver = true;
+        resetGame();
+      })
+    }
   }
 }
-
-
-
 
 // Iniciar el juego
 function startGame() {
   startButton.disabled = true;
+  gameOver = false;
   intervalId = setInterval(() => {
     generateBug();
   }, bugInterval);
@@ -176,6 +166,7 @@ function startGame() {
   setTimeout(increaseBugSpeed, 5000);
 }
 
+
 // Reiniciar el juego
 function resetGame() {
   score = 0;
@@ -184,36 +175,15 @@ function resetGame() {
   timerEl.textContent = timeLeft;
   clearInterval(intervalId);
   clearInterval(timerId);
+  clearTimeout(speedIncreaseId); // Detener el temporizador de aumento de velocidad
   startButton.disabled = false;
   bugs.querySelectorAll('.bug').forEach(bug => {
     bugs.removeChild(bug);
   });
 }
 
-/*/ Verificar si se quedan sin intentos
-function checkLives() {
-  const livesLeft = 10 - bugs.children.length;
-  if (livesLeft === 0) {    
-    Swal.fire({
-      icon: 'error',
-      title: 'Oops...',
-      text: 'Se acabaron los intentos. Fin del juego.',
-    });
-    resetGame();
-  }
-}
-*/
-// Funcion si perdes
-function generateBug() {
-  createBug();
-  if (bugs.children.length >= 10) {
-    Swal.fire({
-      icon: 'error',
-      title: 'Oops...',
-      text: 'Perdiste hay PLAGAA!!.',
-    }).then(() => {
-      resetGame();
-    })} };
+
+
 
 // Agregar eventos a los botones de inicio y reseteo
 startButton.addEventListener('click', startGame);
